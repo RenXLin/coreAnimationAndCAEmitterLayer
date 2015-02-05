b97fb0f212965e80e8409cb9089774a956bbb1cc
RenXLin <renxlin@126.com>
2015-02-05 17:18:48 +0800

    coreAnimation and 粒子系统CAEmitterLayer
    coreAnimation and 粒子系统CAEmitterLayer

---
+++ DemoCoreAnimation/FireworksView.h
@@ -0,0 +1,19 @@
+//
+//  EmitterView.h
+//  CAEmitterDemo
+//
+//  Created by rl on 14-7-7.
+//  Copyright (c) 2014年 rl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+
+@interface FireworksView : UIView{
+ 
+    
+}
+
+- (void)launchFirework;
+
+@end
---
+++ DemoCoreAnimation/RootVC.h
@@ -0,0 +1,15 @@
+//
+//  RootVC.h
+//  DemoCoreAnimation
+//
+//  Created by rl on 14-7-7.
+//  Copyright (c) 2014年 rl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootVC : UIViewController
+
+@property (nonatomic,strong)UIView *_box;
+
+@end
